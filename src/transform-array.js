f909ba2d94d5bfd811a6afe840e7cb3e141d6a5e
Bunchenko <andrew.by.one@gmail.com>
2022-10-16 21:32:38 +0300

    solve hanoi-tower

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,38 +13,36 @@ const { NotImplementedError } = require("../extensions/index.js");
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(arr) {
-	if (!Array.isArray(arr)) {
-		throw Error("'arr' parameter must be an instance of the Array!");
-	}
-	let controlSequences = {
-		"--discard-next": function (array, index) {
-			if (!array[index]) return;
-			array.splice(index, 1);
-		},
-		"--discard-prev": function (array, index) {
-			if (!array[index - 1]) return;
-			array.splice(index - 1, 1);
-		},
-		"--double-next": function (array, index) {
-			if (!array[index]) return;
-			array.splice(index, 1, array[index], array[index]);
-		},
-		"--double-prev": function (array, index) {
-			if (!array[index - 1]) return;
-			array.splice(index - 1, 1, array[index], array[index]);
-		},
-	};
-
-	let resultArr = arr.filter((item) => typeof item !== "string");
-
-	for (let i = 0; i < arr.length; i++) {
-		if (typeof arr[i] === "string" && controlSequences.hasOwnProperty(arr)) {
-			controlSequences[arr[i]](resultArr, i);
-		}
-	}
-
-	return resultArr;
+function transform(/* arr */) {
+	throw new NotImplementedError("Not implemented");
+	// if (!Array.isArray(arr)) {
+	// 	throw Error("'arr' parameter must be an instance of the Array!");
+	// }
+	// let controlSequences = {
+	// 	"--discard-next": function (array, index) {
+	// 		if (!array[index]) return;
+	// 		array.splice(index, 1);
+	// 	},
+	// 	"--discard-prev": function (array, index) {
+	// 		if (!array[index - 1]) return;
+	// 		array.splice(index - 1, 1);
+	// 	},
+	// 	"--double-next": function (array, index) {
+	// 		if (!array[index]) return;
+	// 		array.splice(index, 1, array[index], array[index]);
+	// 	},
+	// 	"--double-prev": function (array, index) {
+	// 		if (!array[index - 1]) return;
+	// 		array.splice(index - 1, 1, array[index], array[index]);
+	// 	},
+	// };
+	// let resultArr = arr.filter((item) => typeof item !== "string");
+	// for (let i = 0; i < arr.length; i++) {
+	// 	if (typeof arr[i] === "string" && controlSequences.hasOwnProperty(arr)) {
+	// 		controlSequences[arr[i]](resultArr, i);
+	// 	}
+	// }
+	// return resultArr;
 }
 
 module.exports = {
